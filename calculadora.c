cde498332c8378f3087f4ea68bb67eabcdf080fb
KELVEM123 <kelvennicolas667@gmail.com>
2025-10-21 14:37:31 +0000

    lnhb

---
+++ calculadora.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+double somar(double num_a, double num_b) {
+    return num_a + num_b;
+}
+
+double subtrair(double num_a, double  num_b) {
+    return num_a - num_b;
+}
+
+int main() {
+    double num_1, num_2; 
+    int operacao
+    double resultado;
+
+    printf("Digite um número: ");
+    scanf("%lf", &num_1);
+
+    printf("Digite um número: ");
+    scanf("%lf", &num_2);  
+      return 0;
+}
---
+++ constantes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define tamanho 10
+
+const i  MAX_SIZE = 50;
+
+int main() {
+    char palavra[TAMANHO];
+    char palavra_2[MAX_SIZE];
+
+    const char data = "21/10/2025";
+    
+    return 0
+}
